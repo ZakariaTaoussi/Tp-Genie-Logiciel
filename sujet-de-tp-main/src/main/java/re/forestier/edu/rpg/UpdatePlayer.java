74838614bd2a064e394f0dbb6082eb798405df3e
ZakariaTaoussi <zakaria.taoussi03@gmail.com>
2025-11-27 18:07:06 +0100

    Refactoring majFinDeTour et AddXp

--- sujet-de-tp-main/src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
+++ sujet-de-tp-main/src/main/java/re/forestier/edu/rpg/UpdatePlayer.java
@@ -5,7 +5,7 @@ import java.util.Random;
 
 public class UpdatePlayer {
 
-    private final static String[] objectList = {"Lookout Ring : Prevents surprise attacks","Scroll of Stupidity : INT-2 when applied to an enemy", "Draupnir : Increases XP gained by 100%", "Magic Charm : Magic +10 for 5 rounds", "Rune Staff of Curse : May burn your ennemies... Or yourself. Who knows?", "Combat Edge : Well, that's an edge", "Holy Elixir : Recover your HP"
+    private final static String[] objectList = {"Lookout Ring : Prevents surprise attacks", "Scroll of Stupidity : INT-2 when applied to an enemy", "Draupnir : Increases XP gained by 100%", "Magic Charm : Magic +10 for 5 rounds", "Rune Staff of Curse : May burn your ennemies... Or yourself. Who knows?", "Combat Edge : Well, that's an edge", "Holy Elixir : Recover your HP"
     };
 
     public static HashMap<String, HashMap<Integer, HashMap<String, Integer>>> abilitiesPerTypeAndLevel() {
@@ -104,63 +104,55 @@ public class UpdatePlayer {
         int newLevel = player.retrieveLevel();
 
         if (newLevel != currentLevel) {
-            // Player leveled-up!
-            // Give a random object
-            ;
             Random random = new Random();
-            player.inventory.add(objectList[random.nextInt(objectList.length - 0) + 0]);
-
-            // Add/upgrade abilities to player
+            player.inventory.add(objectList[random.nextInt(objectList.length)]); // Simplification de l'argument
             HashMap<String, Integer> abilities = abilitiesPerTypeAndLevel().get(player.getAvatarClass()).get(newLevel);
             abilities.forEach((ability, level) -> {
-                player.abilities.put(ability, abilities.get(ability));
+                player.abilities.put(ability, level);
             });
             return true;
         }
         return false;
     }
 
-    // majFinDeTour met à jour les points de vie
+
+
     public static void majFinDeTour(player player) {
         if(player.currenthealthpoints == 0) {
             System.out.println("Le joueur est KO !");
             return;
         }
-
-        if(player.currenthealthpoints < player.healthpoints/2) {
-            if(!player.getAvatarClass().equals("ADVENTURER")) {
-                if(player.getAvatarClass().equals("DWARF")) {
-                    if(player.inventory.contains("Holy Elixir")) {
-                        player.currenthealthpoints+=1;
-                    }
-                    player.currenthealthpoints+=1;
-                } else if(player.getAvatarClass().equals("ADVENTURER")) {
-                    player.currenthealthpoints+=2;
-                }
-
-
-                if(player.getAvatarClass().equals("ARCHER")) {
-                    player.currenthealthpoints+=1;
-                    if(player.inventory.contains("Magic Bow")) {
-                        player.currenthealthpoints+=player.currenthealthpoints/8-1;
-                    }
-                }
-            } else {
-                player.currenthealthpoints+=2;
-                if(player.retrieveLevel() < 3) {
-                    player.currenthealthpoints-=1;
-                }
-            }
-        } else if(player.currenthealthpoints >= player.healthpoints/2){
-            if(player.currenthealthpoints >= player.healthpoints) {
-                player.currenthealthpoints = player.healthpoints;
-                return;
-            }
-        }
-
-
-        if(player.currenthealthpoints >= player.healthpoints) {
+        if (player.currenthealthpoints >= player.healthpoints) {
             player.currenthealthpoints = player.healthpoints;
+            return;
         }
+        if(player.currenthealthpoints < player.healthpoints / 2) {
+            final String avatarClass = player.getAvatarClass();
+
+            switch (avatarClass) {
+                case "DWARF":
+                    player.currenthealthpoints += 1;
+                    if (player.inventory.contains("Holy Elixir")) {
+                        player.currenthealthpoints += 1;
+                    }
+                    break;
+                case "ARCHER":
+                    player.currenthealthpoints += 1;
+                    if (player.inventory.contains("Magic Bow")) {
+                        player.currenthealthpoints += player.currenthealthpoints / 8 - 1;
+                    }
+                    break;
+
+                case "ADVENTURER":
+                    player.currenthealthpoints += 2;
+                    if (player.retrieveLevel() < 3) {
+                        player.currenthealthpoints -= 1;
+                    }
+                    break;
+            }
+        }
+
     }
+
 }
+
