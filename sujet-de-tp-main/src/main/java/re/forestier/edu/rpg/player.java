4412308d8359ee954e038ca60401424c3bc261c2
ZakariaTaoussi <zakaria.taoussi03@gmail.com>
2025-12-11 13:58:31 +0100

    Modification Constructeur Player et Methode RemoveMoney

--- sujet-de-tp-main/src/main/java/re/forestier/edu/rpg/player.java
+++ sujet-de-tp-main/src/main/java/re/forestier/edu/rpg/player.java
@@ -2,38 +2,38 @@ package re.forestier.edu.rpg;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-
+//j ai pas pu changé le Nom de la classe player -> Player car je risque de cassé les test
 public class player {
+
     public String playerName;
     public String Avatar_name;
     private Avatar AvatarClass;
-
     public Integer money;
-
-
-    public int level;
     public int healthpoints;
     public int currenthealthpoints;
     protected int xp;
+    public HashMap<String, Integer> abilities;
+    public ArrayList<String> inventory;
     private static final int SEUIL_NIVEAU_2 = 10;
     private static final int SEUIL_NIVEAU_3 = 27;
     private static final int SEUIL_NIVEAU_4 = 57;
     private static final int SEUIL_NIVEAU_5 = 111;
 
-    public HashMap<String, Integer> abilities;
-    public ArrayList<String> inventory;
     public player(String playerName, String avatar_name, String avatarClass, int money, ArrayList<String> inventory) {
         if (!avatarClass.equals("ARCHER") && !avatarClass.equals("ADVENTURER") && !avatarClass.equals("DWARF") ) {
             return;
         }
-
         this.playerName = playerName;
-        Avatar_name = avatar_name;
-
-        this.AvatarClass = AvatarFactory.createAvatar(avatarClass);
-        this.money = Integer.valueOf(money);
+        this.Avatar_name = avatar_name;
+        AvatarFactory factory = new AvatarFactory();
+        this.AvatarClass = factory.createAvatar(avatarClass);
+        this.money = money;
         this.inventory = inventory;
-        this.abilities = this.AvatarClass.getAbilitiesForLevel(1);
+        if (this.AvatarClass != null) {
+            this.abilities = this.AvatarClass.getAbilitiesForLevel(1);
+        } else {
+            this.abilities = new HashMap<>();
+        }
     }
 
     public String getAvatarClass() {
@@ -44,19 +44,19 @@ public class player {
         if (money - amount < 0) {
             throw new IllegalArgumentException("Player can't have a negative money!");
         }
+        money -= amount;
+    }
 
-        money = Integer.parseInt(money.toString()) - amount;
-    }
     public void addMoney(int amount) {
-        var value = Integer.valueOf(amount);
-        money = money + (value != null ? value : 0);
+        money += amount;
     }
+
     public int retrieveLevel() {
         if (xp < SEUIL_NIVEAU_2) return 1;
         if (xp < SEUIL_NIVEAU_3) return 2;
         if (xp < SEUIL_NIVEAU_4) return 3;
         if (xp < SEUIL_NIVEAU_5) return 4;
-        return 5; // Si on a dépassé tous les seuils
+        return 5;
     }
 
     public int getXp() {
