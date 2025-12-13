976e9e85b78f56bf396f0c6723ab59563c6453b2
ZakariaTaoussi <zakaria.taoussi03@gmail.com>
2025-12-13 18:33:45 +0100

    Creation des Fichier pour structure le projet correcton de bug lvl 2 adventurer ajoute de la class Goblin avec les Abilité mentionné dans le tp

---
+++ sujet-de-tp-main/src/main/java/re/forestier/edu/rpg/Avatar/Goblin.java
@@ -0,0 +1,47 @@
+package re.forestier.edu.rpg;
+
+import java.util.HashMap;
+
+public class Goblin implements Avatar {
+
+    private static final String TYPE = "GOBLIN";
+
+    @Override
+    public String getType() {
+        return TYPE;
+    }
+
+    @Override
+    public int HealthRegen(player player) {
+        // Aucune spécification du prof : on retourne 1 (valeur par défaut standard)
+        return 1;
+    }
+
+    @Override
+    public HashMap<String, Integer> getAbilitiesForLevel(int level) {
+        HashMap<String, Integer> stats = new HashMap<>();
+
+        switch (level) {
+            case 1 -> {
+                stats.put("INT", 2);
+                stats.put("ATK", 2);
+                stats.put("ALC", 1);
+            }
+            case 2 -> {
+                stats.put("ATK", 3);
+                stats.put("ALC", 4);
+            }
+            case 3 -> stats.put("VIS", 1);
+            case 4 -> stats.put("DEF", 1);
+            case 5 -> {
+                stats.put("DEF", 2);
+                stats.put("ATK", 4);
+            }
+            default -> {
+                // rien
+            }
+        }
+
+        return stats;
+    }
+}
--- sujet-de-tp-main/src/main/java/re/forestier/edu/rpg/player.java
+++ sujet-de-tp-main/src/main/java/re/forestier/edu/rpg/player.java
@@ -50,5 +50,6 @@ public class player {
 
     public void removeMoney(int amount) throws IllegalArgumentException {
         this.money = WalletManager.remove(this.money, amount);
+
     }
 }
